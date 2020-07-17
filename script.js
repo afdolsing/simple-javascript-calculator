a001d0f3b6f23d1f3fc7bf44f6342c9fd82ee723
afdolsing <singgih.student@gmail.com>
2020-07-18 05:54:39 +0700

    update

--- script.js
+++ script.js
@@ -8,14 +8,11 @@ button.addEventListener("click",function(e){
 
     if(buttonValue == "C"){
         screen.value = ""
-    }
-    else if(buttonValue == "←"){
+    } else if(buttonValue == "←"){
         screen.value = screen.value.slice(0, -1)
-    }
-    else if(buttonValue == "="){
+    } else if(buttonValue == "="){
         screen.value = eval(screen.value)
-    }
-    else{
+    } else{
         screen.value = screen.value + buttonValue
     }
 })
